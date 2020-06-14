b7027c060886d7e56cba3e8a70abe0f017d18338
Rahul Rao <nkrahul88@gmail.com>
2020-06-14 17:58:22 +0530

    Hello World in Java

--- helloworld.java
+++ helloworld.java
@@ -1,10 +1,10 @@
-public class Hello {
+public class helloworld{
 
-    public static void main(string[] args) {
+    public static void main(String[] args){
 
-        for(i=1;i<=10;i++){
+        for(int i=1;i<=10;i++){
 
-            system.out.println"(Hello World"+i);
+            System.out.println("Hello World"+i);
 
         }
     }
