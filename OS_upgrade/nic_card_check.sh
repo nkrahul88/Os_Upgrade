c86fef1e9d2cb88b5bb17601b0ba0b4a2787463f
Rahul Rao <nkrahul88@gmail.com>
2020-10-19 19:55:30 +0530

    NIC Connectivity & status check script

---
+++ OS_upgrade/nic_card_check.sh
@@ -0,0 +1,10 @@
+for i in `ip a s |grep UP | awk -F ':' '{print $2}' |egrep 'eno|ens|eth|bond'|grep -v "@"`
+    do
+        echo -e "Checking for NIC :  "$i""
+        result=`ethtool $i | grep Link | awk '/:/ {print $3}'`
+        if [ "$result" == yes ] ; then
+                echo -e "[INFO]\t Link is UP for $i\n"
+        else
+                echo -e "[ALERT]\t Link is DOWN for $i\n"
+        fi
+    done
