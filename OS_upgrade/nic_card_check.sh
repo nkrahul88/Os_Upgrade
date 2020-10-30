07699c40fd8d57a718b3cbf815b4a6221abe7347
Rahul Rao <nkrahul88@gmail.com>
2020-10-30 16:32:38 +0530

    Adding steps to fetch speed, duplex & driver info

--- OS_upgrade/nic_card_check.sh
+++ OS_upgrade/nic_card_check.sh
@@ -1,10 +1,12 @@
 for i in `ip a s |grep UP | awk -F ':' '{print $2}' |egrep 'eno|ens|eth|bond'|grep -v "@"`
     do
         echo -e "Checking for NIC :  "$i""
-        result=`ethtool $i | grep Link | awk '/:/ {print $3}'`
+		        result=`ethtool $i | grep Link | awk '/:/ {print $3}'`
         if [ "$result" == yes ] ; then
                 echo -e "[INFO]\t Link is UP for $i\n"
         else
                 echo -e "[ALERT]\t Link is DOWN for $i\n"
         fi
-    done
+	ethtool $i | egrep 'Speed|Duplex'
+	ethtool -i $i | egrep 'driver|version' | head -2
+done
